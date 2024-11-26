b6096e891d721405d5ed4e774422e584636937f3
Dwain Madolid <dwainmadolid@hotmail.co.uk>
2024-11-26 16:32:25 +0000

    created navbar and added simple css styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/header";
+import Home from "./components/home";
 import FindArt from "./components/findArt";
 import Login from "./components/login";
 import Exhibitions from "./components/viewExhibitions";
@@ -11,6 +12,7 @@ function App() {
       <>DEE WIZ</>
       <Header />
       <Routes>
+        <Route path="/home" element={<Home />} />
         <Route path="/artwork" element={<FindArt />} />
         <Route path="/login" element={<Login />} />
         <Route path="/exhibitions" element={<Exhibitions />} />
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,14 +5,17 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <nav >
-        <Link to="/artwork">
+      <nav className="navbar">
+        <Link to="/home" className="navbar-item">
+          HOME
+        </Link>
+        <Link to="/artwork" className="navbar-item">
           Find Art
         </Link>
-        <Link to="/exhibitions">
+        <Link to="/exhibitions" className="navbar-item">
           View Exhibitions
         </Link>
-        <Link to="/login">
+        <Link to="/login" className="navbar-item">
           Login
         </Link>
       </nav>
